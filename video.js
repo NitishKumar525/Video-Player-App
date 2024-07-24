4f0028499d7e946280fe04b76182d0feda6ca5ee
Nitish Kumar Jha <nitishjha525@gmail.com>
2024-07-24 08:54:05 +0530

    Update video.js

--- video.js
+++ video.js
@@ -8,7 +8,7 @@ const playerSpeed = document.querySelector(".player-speed");
 const fullscreen = document.getElementById("fullScreen-btn");
 const volumeContainer = document.querySelector(".volume");
 const volumeBar = document.querySelector(".volume-bar");
-const volBtn = document.getElementById("volume-btn");hey siri play a song fo
+const volBtn = document.getElementById("volume-btn");hey siri play a 
 
 function fnMute() {
   if (videoPlayer.volume) {
